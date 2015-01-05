98d30c18443b9d4e9fec216926fcd123eb5cced7
Matt Jones <matt.jones@smsmt.com>
2015-01-05 13:43:17 +1100

    Added menu-bar and navigation controller

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,25 @@
 (function() {
 	var app = angular.module("clientXray", []);
 
-	{{ 1 + 2 }}
-  
+	app.directive("menuBar", function() {
+		return {
+			restrict: 'E',
+			templateUrl: '../menu-bar.html'
+		};
+	});
+
+	app.controller('NavigationController', function($scope){
+	    // Default panel here
+	    $scope.activePanel = "login";
+	    
+	    $scope.setPanel = function(newPanel) {
+	      $scope.activePanel = newPanel;
+	    };
+	    
+	    $scope.isSet = function(panelName){
+	      return $scope.activePanel === panelName;
+	    };
+    
+  	});
+  	
 })();
