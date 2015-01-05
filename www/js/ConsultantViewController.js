81c1b96e9f7ecc4c6a8c6ededb026aadd8a2f844
Henry Niu <hniu@smsmt.com>
2015-01-05 15:56:05 +1100

    Change mood description

--- www/js/ConsultantViewController.js
+++ www/js/ConsultantViewController.js
@@ -1,13 +1,13 @@
 var ConsultantViewController = function ($scope, $location) {
 	$scope.consultants = [
-	    {name: 'Henry Niu', mood: 'happy', date: '01/2014'},
-	    {name: 'Henry Niu', mood: 'neutral', date: '02/2014'},
-	    {name: 'Henry Niu', mood: 'sad', date: '03/2014'},
-	    {name: 'Henry Niu', mood: 'unkonwn', date: '04/2014'},
-	    {name: 'Matt Jones', mood: 'unknown', date: '01/2014'},
-	    {name: 'Matt Jones', mood: 'sad', date: '02/2014'},
-	    {name: 'Matt Jones', mood: 'neutral', date: '03/2014'},
-	    {name: 'Matt Jones', mood: 'happy', date: '04/2014'}
+	    {name: 'Henry Niu', mood: 'Happy', date: '01/2014'},
+	    {name: 'Henry Niu', mood: ' Indifferent', date: '02/2014'},
+	    {name: 'Henry Niu', mood: ' Postal', date: '03/2014'},
+	    {name: 'Henry Niu', mood: ' Bored', date: '04/2014'},
+	    {name: 'Matt Jones', mood: ' Bored', date: '01/2014'},
+	    {name: 'Matt Jones', mood: ' Postal', date: '02/2014'},
+	    {name: 'Matt Jones', mood: ' Indifferent', date: '03/2014'},
+	    {name: 'Matt Jones', mood: 'Happy', date: '04/2014'}
 	];
 
 	$scope.name = $location.search()['name'];
