8b7b9c646dc91d04b3cb3cb3e13dc5459128561a
fei.you <fei.you@enett.com>
2015-01-06 10:58:39 +1100

    search result drill down.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -63,18 +63,6 @@
 	});
 
 	app.controller('ConsultantViewController', function($scope, consultantData) {
-		/*$scope.consultants = [
-		    {name: 'Henry Niu', mood: 'Happy', date: '01/2014'},
-		    {name: 'Henry Niu', mood: ' Indifferent', date: '02/2014'},
-		    {name: 'Henry Niu', mood: ' Postal', date: '03/2014'},
-		    {name: 'Henry Niu', mood: ' Bored', date: '04/2014'},
-		    {name: 'Matt Jones', mood: ' Bored', date: '01/2014'},
-		    {name: 'Matt Jones', mood: ' Postal', date: '02/2014'},
-		    {name: 'Matt Jones', mood: ' Indifferent', date: '03/2014'},
-		    {name: 'Matt Jones', mood: 'Happy', date: '04/2014'}
-		];
-
-		$scope.name = $location.search()['name'];*/
 		$scope.$watch(
 			function () { 
 				return consultantData.getConsultant(); 
