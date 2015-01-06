396d6e2b6a40993f907320cfc370cc6ce8d66128
Henry Niu <hniu@smsmt.com>
2015-01-06 11:04:54 +1100

    Change consultant view to use data from database

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -63,19 +63,17 @@
 	});
 
 	app.controller('ConsultantViewController', function($scope, consultantData) {
-		/*$scope.consultants = [
-		    {name: 'Henry Niu', mood: 'Happy', date: '01/2014'},
-		    {name: 'Henry Niu', mood: ' Indifferent', date: '02/2014'},
-		    {name: 'Henry Niu', mood: ' Postal', date: '03/2014'},
-		    {name: 'Henry Niu', mood: ' Bored', date: '04/2014'},
-		    {name: 'Matt Jones', mood: ' Bored', date: '01/2014'},
-		    {name: 'Matt Jones', mood: ' Postal', date: '02/2014'},
-		    {name: 'Matt Jones', mood: ' Indifferent', date: '03/2014'},
-		    {name: 'Matt Jones', mood: 'Happy', date: '04/2014'}
-		];
 
-		$scope.name = $location.search()['name'];*/
-		$scope.$watch(
+		$scope.name = $location.search()['name'];
+
+		$scope.search = function(){		
+ 			console.log("Consultant Name: " + name);
+ 			consultantData.getConsultantView(name).success(function(data){			
+ 				$scope.moods = data;
+			});
+		}	
+
+		/*	$scope.$watch(
 			function () { 
 				return consultantData.getConsultant(); 
 			},
@@ -83,7 +81,7 @@
         		if (newValue) 
         			$scope.consultants = newValue;
     		}
-    	);
+    	);	*/
 	}); 
 
 })();
--- www/js/factories.js
+++ www/js/factories.js
@@ -41,25 +41,11 @@
 			});
 		};
   		
-		factory.getConsultant = function(name) {
-			/*return $http({
+		factory.getConsultantView = function(name) {
+			return $http({
 				method : 'GET',
-				url : 'http://hackathonapi-env.elasticbeanstalk.com/v1/'+ searchType.value +'/' + email + "/clients"
-			});*/
-
-  			var consultant = [
-			    {name: 'Henry Niu', mood: 'Happy', date: '01/2014'},
-			    {name: 'Henry Niu', mood: ' Indifferent', date: '02/2014'},
-			    {name: 'Henry Niu', mood: ' Postal', date: '03/2014'},
-			    {name: 'Henry Niu', mood: ' Bored', date: '04/2014'},
-			    {name: 'Matt Jones', mood: ' Bored', date: '01/2014'},
-			    {name: 'Matt Jones', mood: ' Postal', date: '02/2014'},
-			    {name: 'Matt Jones', mood: ' Indifferent', date: '03/2014'},
-			    {name: 'Matt Jones', mood: 'Happy', date: '04/2014'}
-			];
-
-			return consultant;
-
+				url : 'http://hackathonapi-env.elasticbeanstalk.com/v1/consultant/'+ name
+			});
 		};
 
   		return factory;
